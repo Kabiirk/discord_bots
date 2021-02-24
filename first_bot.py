a2b334dd856642c176c6043e2014b1a192433eaf
Kabiir <kabiirk@gmail.com>
2021-02-24 22:56:30 +0530

    Fixed commands issue

--- first_bot.py
+++ first_bot.py
@@ -6,12 +6,25 @@ from urllib import parse as urlparse
 client = discord.Client()
 
 # Functions executed in the program go here
+# ======================================================================================================================
 def url_check(url):
     # check wether URL format if correct or not
     url_scheme = urlparse.urlparse(url).scheme
     # e.g. of result
     # urlparse.urlparse('https://www.youtube.com/')
     # ParseResult(scheme='https', netloc='www.youtube.com', path='/', params='', query='', fragment='')
+
+    
+    if(len(url_scheme)<=0):
+        return False, "This is a links-only channel, Kindly only post valid https links only."
+        # url_scheme empty for normal text
+        # e.g. 
+        # urlparse.urlparse('$jokeasgf')
+        # ParseResult(scheme='', netloc='', path='', params='', query='', fragment='')
+        # scheme for normal strings would be an empty string, exits the function before
+        # the below if-else block is even executed so that the funtion doesn't return
+        # "This bot only allows links following https scheme." which is also a valid condition
+        #  for ftp, http etc links & therefore a different usecase than a normal string.
 
     # Only allows https URLs with a valid response
     if(url_scheme != 'https'):
@@ -57,7 +70,7 @@ def get_joke():
     joke = json_data['setup'] + '\n\n' + json_data['punchline']
     
     return joke
-
+# ======================================================================================================================
 
 # Main Implementation
 @client.event
@@ -68,32 +81,31 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    # ignore if the message is from the bot itserl client.user reffers to the bot
+    # (which is the user in this case)
     if message.author==client.user:
         return
+    
+    # Take only the first word
+    first_word = message.content.split(" ", 1)[0] 
 
-    valid_url, error_text = url_check(message.content)
     # boolean, string
+    valid_url, error_text = url_check(message.content)
         
+    # Don't do anything to the message if it a valid url
     if valid_url:
         pass
     
     else:
         # Functions that the bot performs on the server go here
-
-        # ignore if the message is from the bot itserl client.user reffers to the bot
-        # (which is the user in this case)
-        if message.author==client.user:
-            return
-
-        elif message.content.startswith('$inspire'):
+        if(first_word=='$inspire'):
             quote = get_quote()
             await message.channel.send(quote)
             return
-            
-        elif message.content.startswith('$greet'):
+        elif first_word=='$greet':
             await message.channel.send('Hello '+str(message.author)+' !'+'\n'+'You can look up my documentation @ : '+'https://github.com/Kabiirk/discord_bots')
             return
-        elif message.content.startswith('$xkcd'):
+        elif first_word=='$xkcd':
             comic_id = message.content.split(" ",1)[1]
             if(comic_id == '0'):
                 await message.channel.send("Comic stip not indexable, try another number")
@@ -101,15 +113,15 @@ async def on_message(message):
                 comic_img_link = get_xkcd(comic_id)
                 await message.channel.send(comic_img_link)
             return
-        elif message.content.startswith('$joke'):
+        elif first_word=='$joke':
             joke = get_joke()
             await message.channel.send(joke)
             return
-        # END
 
-        # Deletes messages if they don't begin with $inspire or $greet or don't contain links
+        # Deletes messages whose first word isn't $inspire, $greet etc. or aren't links
         await message.delete()
-        await message.channel.send(error_text)
+        if(error_text != None):
+            await message.channel.send(error_text)
 
 
-client.run('Paste your Token Here')
+client.run('ODEzMDU0NzgwNDQ3OTE2MDYz.YDJuRQ.TFcpadpzKncYsmbs10KNzsGieHo')
